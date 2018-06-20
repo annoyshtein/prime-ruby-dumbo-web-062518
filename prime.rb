d80f2bcbf7d018bd5e68692ab782f524301c45b3
Alex Neustein <alex.neustein@gmail.com>
2018-06-20 22:21:27 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,8 @@
 def prime? (num)
   return false if num <= 1
-  number_range = 2..num
-  
+  number_range = 2..num.
+  for shoe in shoe_count 
+    puts "I'm putting a pair of shoes away."
+end
   
 end
