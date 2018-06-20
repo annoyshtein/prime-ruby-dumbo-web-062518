cfb7d18087403a68cb61bd8093da35a0abde9cef
Alex Neustein <alex.neustein@gmail.com>
2018-06-20 22:31:25 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,8 @@
 def prime? (num)
   puts "Number is less than 1" if num <= 1
   return false if num <= 1
+  puts "2 is a prime number" if num == 2
+  return true if num == 2
   test_up_to = Math.sqrt(num).to_i
   puts "We are testing factors up to #{test_up_to}."
   factor_array = (2..test_up_to).to_a
